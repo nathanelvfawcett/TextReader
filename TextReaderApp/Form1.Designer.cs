3d32409af15e9493a89e65cc18edcaa9a72ecf8f
Nathan Fawcett <44716057+nFawcettPortfolio@users.noreply.github.com>
2020-03-19 14:06:41 -0400

    Add files via upload

---
+++ TextReaderApp/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace TextReaderApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.readButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Location = new System.Drawing.Point(47, 12);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(75, 23);
+            this.uploadButton.TabIndex = 0;
+            this.uploadButton.Text = "Upload";
+            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.Click += new System.EventHandler(this.uploadButton_Click);
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(157, 12);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(75, 23);
+            this.readButton.TabIndex = 1;
+            this.readButton.Text = "Read";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 43);
+            this.Controls.Add(this.readButton);
+            this.Controls.Add(this.uploadButton);
+            this.Name = "Form1";
+            this.Text = "Text Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.Button readButton;
+    }
+}
+
---
+++ TextReaderApp/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextReaderApp
+{
+    public partial class Form1 : Form
+    {
+        string fileName;
+        string line;
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize a new instance of the SpeechSynthesizer.  
+
+            // Configure the audio output.   
+            synth.SetOutputToDefaultAudioDevice();
+        }
+
+        private void uploadButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    fileName = dialog.FileName;
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An error occured.");
+            }
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        synth.Speak(line);
+                    }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An error occured. Please try again.");
+            }
+        }
+    }
+}
